ec93395f7ddccc17ed6a234d51f011eacbeaf070
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-03 08:14:27 +0900

    done create_models

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -35,9 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'api.apps.ApiConfig',
 ]
 
 ROOT_URLCONF = 'drfapi.urls'
